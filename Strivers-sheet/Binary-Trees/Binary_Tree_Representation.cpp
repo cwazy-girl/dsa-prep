5ff64f9591471faa491931a637390edc41e04da6
cwazy-girl <avanigupta0207@gmail.com>
2026-05-26 18:24:06 +0530

    Create Binary_Tree_Representation.cpp

---
+++ Strivers-sheet/Binary-Trees/Binary_Tree_Representation.cpp
@@ -0,0 +1,12 @@
+//Binary Tree Representation
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
